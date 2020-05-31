654aad566ec2c4a7c124a7e3749b86a582a0ce17
CC <chuancc@gmail.com>
2020-05-31 22:37:08 +1200

    add locust for load testing.

---
+++ src/locust/locustfile.py
@@ -0,0 +1,29 @@
+import random
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    wait_time = between(5, 9)
+
+    @task
+    def index_page(self):
+        self.client.get("/ping")
+        self.client.get("/version")
+        self.client.get("/air/aqi")
+
+    @task(3)
+    def view_item(self):
+        cities = ["beijing", 
+                    "chengdu", 
+                    "auckland", 
+                    "london", 
+                    "shanghai", 
+                    "tianjing", 
+                    "xian", 
+                    "dalian", 
+                    "shenzheng",
+                    "guangzhou",
+                    "wuhan",
+                    "xiamen"]
+        id = random.randint(0, len(cities)-1)
+        self.client.get("/air/city/"+cities[id], name="/air/city")
+
